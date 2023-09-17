514dc4535c484d163ab64de35811409aabb301a4
rahulml25 <rahulml2505@gmail.com>
2023-09-17 23:23:30 +0530

    Learned: input, output, and variables

---
+++ src/com/input-output - Copy/Main.java
@@ -0,0 +1,11 @@
+package com.apnacollage;
+
+public class Main {
+    public static void main(String[] args) {
+        // output
+        System.out.println("Hello World with Java");
+        System.out.print("Hello World with Java\n");
+        System.out.print("Hello World with Java\nfrom\n Apni Kaksha");
+
+    }
+}
---
+++ src/com/input_output/Main.java
@@ -0,0 +1,55 @@
+package com.input_output;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // // output
+        // System.out.println("Hello World with Java");
+        // System.out.print("Hello World with Java\n");
+        // System.out.print("Hello World with Java\nfrom\n Apni Kaksha");
+
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+
+        // // variables
+        // String name = "tony stark";
+        // int age = 48;
+        // double price = 25.25;
+        // int a = 25;
+        // int b = 10;
+
+        // b = 20;
+        // name = "ironman";
+        // System.out.println();
+
+        // int a = 10;
+        // int b = 25;
+
+        // int sum = a + b;
+        // //System.out.println(sum);
+
+        // int diff = b - a;
+        // //System.out.println(diff);
+
+        // int mul = a*b;
+        // System.out.println(mul);
+
+        // // Input
+        // Scanner sc = new Scanner(System.in);
+        // //String name = sc.next();
+        // String name = sc.nextLine();
+        // //nextInt()
+        // //nextFloat()
+        // System.out.println(name);
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        //System.out.println("sum");
+        System.out.println(sum);
+    }
+}
